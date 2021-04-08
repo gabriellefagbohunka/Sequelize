b3a351c776111724628892009d7986e4fc1703e5
Gabrielle Fagbohunka <gabbyf99@terpmail.umd.edu>
2021-04-08 11:38:53 -0400

    JS update

--- public/script.js
+++ public/script.js
@@ -2,5 +2,15 @@ async function diningHalls() {
     const apif = await fetch('/api/dining')
     const x = await apif.json();
     const arrayd = x.data;
-    const 
+    const y = document.querySelector('.target'); 
+    arrayd.forEach((z) => {
+        const foodVar = document.createElement('tr')
+        foodVar.innerHTML =`
+            <td>${z.hall_id}<td/>
+            <td>${z.hall_name}<td/>
+            <td>${z.hall_location}<td/>`;
+            y.append(foodVar);
+    });
 }
+window.onload = diningHalls();
+
