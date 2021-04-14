e2c251306c34794c674cee9d1f1983544ad19e56
Gabrielle Fagbohunka <gabbyf99@terpmail.umd.edu>
2021-04-14 14:27:50 -0400

    W8 spacing change and CSS

--- public/script.js
+++ public/script.js
@@ -1,15 +1,16 @@
 async function diningHalls() {
-    const apif = await fetch('/api/dining')
+    const apif = await fetch('/api/dining');
     const x = await apif.json();
     const arrayd = x.data;
     const y = document.querySelector('.target'); 
     arrayd.forEach((z) => {
         const foodVar = document.createElement('tr')
         foodVar.innerHTML =`
-            <td>${z.hall_id}<td/>
-            <td>${z.hall_name}<td/>
-            <td>${z.hall_address}<td/>`;
-            y.append(foodVar);
+            <td>${z.hall_id}</td>
+            <td>${z.hall_name}</td>
+            <td>${z.hall_address}</td>
+        `;
+        y.append(foodVar)
     });
 }
 window.onload = diningHalls();
